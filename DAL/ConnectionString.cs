b801bd1cfa17c69911f8d5b93d8436acec2f6234
Ahmed Esam <buhussien2@gmail.com>
2025-08-11 02:50:42 +0300

    Update ConnectionString.cs

--- DAL/ConnectionString.cs
+++ DAL/ConnectionString.cs
@@ -3,5 +3,5 @@ namespace DAL;
 public class ConnectionString
 {
     public static string  connectionString = 
-        "Server=localhost; Database=LibraryManagementDb; User Id=BuHussien; Password=Ahmedesam_DDR5; Encrypt=False;";
+        "Server=localhost; Database=LibraryManagementDb; User Id= *******; Password=***********;";
 }
